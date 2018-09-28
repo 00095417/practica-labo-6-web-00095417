5b61cd6ba0039e2a9ff90449c94ec36b3115aaff
00095417 <00095417@uca.edu.sv>
2018-09-28 02:19:40 -0600

    primer commint

--- Ejercicio2.js
+++ Ejercicio2.js
@@ -1,10 +1,9 @@
 function sumaYPromedio(array){
     let pluss=0;
-    let prom=0;
-    
+
     for(let n of array){
         pluss+=n;
     }
 
-    return [pluss,prom=(pluss/array.length)];
+    return ["suma = " + pluss, "promerdio = " + (pluss/array.length)];
 }
